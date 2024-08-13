9e5ca9c0f0dace451f541e466ac8531a022a27f7
Mahdi <mahdimeyghani02@gmail.com>
2024-08-13 16:09:08 +0330

    Creating model

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEAL_KINDS = (
+    ("starters", "Starters"),
+    ("salads", "Salads"),
+    ("main_dishes", "Main Dishes"),
+    ("deserts", "Deserts"),
+)
+
+AVAILABILITY = (
+    (0, "Unavailable"),
+    (1, "Available"),
+)
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=1500, unique=True)
+    description = models.CharField(max_length=5000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100, choices=MEAL_KINDS)
+    status = models.IntegerField(max_length=10, choices=AVAILABILITY, default=1)
+    cook = models.ForeignKey(User, on_delete=models.PROTECT)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
