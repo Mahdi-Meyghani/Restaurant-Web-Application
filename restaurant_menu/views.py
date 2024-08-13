87c488d2853c0f32ef311e39740ab1806e471b88
Mahdi <mahdimeyghani02@gmail.com>
2024-08-13 22:19:40 +0330

    Expressing types, meals name and ingredients of each meal in the home page

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Meal
+from .models import Meal, MEAL_KINDS
 
 
 class MenuList(generic.ListView):
     queryset = Meal.objects.order_by("-date_created")
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["kinds"] = MEAL_KINDS
+        return context
 
 
 class MenuDetail(generic.DetailView):
